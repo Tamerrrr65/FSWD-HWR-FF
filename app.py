8b5bda578d11cf58a75725169b349fd61f12b435
Tamer Abu Hweidi <s_abuhweidi23@stud.hwr-berlin.de>
2024-11-21 15:21:39 +0100

    Registriren und Anmelden hinzugefügt

--- app.py
+++ app.py
@@ -14,5 +14,13 @@ def second_page():
 def third_page():
     return render_template('Test.html')
 
+@app.route('/Anmelden')
+def fourth_page():
+    return render_template('Anmelden.html')
+
+@app.route('/Registrieren')
+def fifth_page():
+    return render_template('Registrieren.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
